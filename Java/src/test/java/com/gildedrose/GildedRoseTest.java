55b878c703272e9a6d158402cbaa44068565b735
fabiuzsocial <fabiuz.social@gmail.com>
2021-01-05 17:49:33 +0100

    Added approvalstest library and changed the test using combination tool

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,30 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testUpdateQuality() {
+
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[]{"foo"},
+                new Integer[]{0},
+                new Integer[]{0}
+        );
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        //assertEquals("foo", app.items[0].name);
+        String itemAsAString = app.items[0].toString();
+        return itemAsAString;
     }
 
 }
